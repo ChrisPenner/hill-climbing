7743c00012652bddaecbfb44811368b22ad25c2c
Chris Penner <christopher.penner@gmail.com>
2018-11-16 18:11:30 -0600

    Simple topology climber

--- README.md
+++ README.md
@@ -6,4 +6,18 @@
 [![Stackage Nightly](http://stackage.org/package/hill-climbing/badge/nightly)](http://stackage.org/nightly/package/hill-climbing)
 [![Build status](https://secure.travis-ci.org/ChrisPenner/hill-climbing.svg)](https://travis-ci.org/ChrisPenner/hill-climbing)
 
-Hill climbing algorithm using Comonads
+Hill climbing algorithm using Store Comonads
+
+This provides some fun functions for finding local maxima in simple topologies
+
+`showSteps delta n t` will show `n` steps it takes towards the local maxima
+checking the surrounding values displaced by `delta` orthogonally from the
+current position as a nested tuple `((x, y), value)`
+
+```haskell
+λ> showSteps 0.1 10 (topology (0.3, 0.2))
+[((0.3,0.2),0.2896295),((0.4,0.2),0.3816559),((0.5,0.2),0.46986896),((0.6,0.2),0.5533
+872),((0.70000005,0.2),0.63137627),((0.8000001,0.2),0.7030568),((0.9000001,0.2),0.767
+7126),((1.0000001,0.2),0.8246977),((1.1000001,0.2),0.8734426),((1.2000002,0.2),0.9134
+6043)]
+```
--- src/HillClimbing.hs
+++ src/HillClimbing.hs
@@ -1,6 +1,69 @@
-module HillClimbing
-       ( someFunc
-       ) where
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE TypeFamilies #-}
+{-# LANGUAGE TupleSections #-}
+module HillClimbing where
 
-someFunc :: IO ()
-someFunc = putStrLn ("someFunc" :: String)
+import           Control.Comonad.Store
+import           Data.Bifunctor
+import           Data.Distributive
+import           Data.Functor.Rep
+import           Data.Function
+import           Data.Foldable
+import Control.Arrow ((&&&))
+
+
+-- topology :: Store (Float, Float) Float
+-- topology = store polynomial (5, 5)
+--  where
+--   polynomial (x, y) = sin (sqrt (x ** 2 + y ** 2)) / sqrt (x ** 2 + y ** 2)
+
+
+topology :: (Float, Float) -> Store (Float, Float) Float
+topology startPos = store polynomial startPos
+  where polynomial (x, y) = sin x * cos y
+
+data Nearby a = Nearby {up :: a, right :: a, down :: a, left :: a}
+  deriving (Functor, Show, Eq, Foldable)
+
+data Dir = U | D | R | L deriving (Eq, Show)
+
+instance Distributive Nearby where
+  distribute = distributeRep
+
+instance Representable Nearby where
+  type Rep Nearby = Dir
+  index n U = up n
+  index n D = down n
+  index n R = right n
+  index n L = left n
+
+  tabulate f = Nearby {up=f U, down=f D, right=f R, left=f L}
+
+nearby :: Float -> (Float, Float) -> Nearby (Float, Float)
+nearby = distribute . tabulate . move
+
+move :: Float -> Dir -> (Float, Float) -> (Float, Float)
+move delta U = second (subtract delta)
+move delta D = second (+ delta)
+move delta L = first (subtract delta)
+move delta R = first (+ delta)
+
+step :: Float -> Store (Float, Float) Float -> Store (Float, Float) Float
+step delta w =
+  let nearbyValues = w & experiment (nearby delta)
+      bestDirection =
+        fst . maximumBy (compare `on` snd) . withIndex $ nearbyValues
+  in  flip seek w $ move delta bestDirection (pos w)
+  where withIndex = imapRep (,)
+
+steps
+  :: Float -> Int -> Store (Float, Float) Float -> Store (Float, Float) Float
+steps delta n w = nTimes n (step delta) w
+
+showSteps
+  :: Float -> Int -> Store (Float, Float) Float -> [((Float, Float), Float)]
+showSteps delta n w = (pos &&& extract) <$> (take n $ iterate (step delta) w)
+
+nTimes :: Int -> (a -> a) -> a -> a
+nTimes n f = head . drop n . iterate f
